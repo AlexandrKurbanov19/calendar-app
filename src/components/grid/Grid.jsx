efb42a0b77aa786ca136127ca37e4ae85c606dd4
evgeny.ivashov <evgeny.ivashov@adbusiness.biz>
2022-02-15 14:01:03 +0700

    добавлена ф-я замена классов

--- src/components/grid/Grid.jsx
+++ src/components/grid/Grid.jsx
@@ -1,44 +1,57 @@
-import moment from 'moment';
-import React from 'react';
-import '../../index.scss';
-import uniqid from 'uniqid';
+import moment from "moment";
+import React from "react";
+import "../../index.scss";
+import uniqid from "uniqid";
 
+export default function Grid({ startDay, totalDays, events }) {
+  const day = startDay.clone().subtract(1, "day");
+  const daysMap = [...Array(totalDays)].map(() => day.add(1, "day").clone());
 
-export default function Grid({startDay, totalDays, events}) {
-
+  // проверка на текущую дату, прошедшие дни и выходные
+  const isCurrentDay = (day) => moment().isSame(day, "day");
+  const isAfterDay = (day) => moment().isAfter(day, "day");
+  const isWeekend = (day) => day.day() === 0 || day.day() === 6;
   
-	const day = startDay.clone().subtract(1, 'day');
-	const daysMap = [...Array(totalDays)].map(() => day.add(1, 'day').clone())
-  // проверка на текущую дату
-  const isCurrentDay = (day) => moment().isSame(day, 'day');
-  
+  // возвращаю нужный клас
+  const changeClassName = (day) => {
+    if (isCurrentDay(day)) {
+      return "current";
+    } else if (isAfterDay(day)) {
+      return "last";
+    } else if (isWeekend(day)) {
+      return "weekend";
+    }
+  };
 
   return (
-    <div className='grid'>
+    <div className="grid">
       {daysMap.map((weekItem) => (
-        <div  key={uniqid()} className={moment().isSame(weekItem, 'day') ? "grid__item current" : "grid__item" && moment().isAfter(weekItem, 'day') ? "grid__item last" : "grid__item"  && (weekItem.day() === 0 || weekItem.day() === 6 ? "grid__item weekend" : "grid__item")} >
-        
-        <div className='grid__item-card'>
-        {
-											isCurrentDay(weekItem) ? (
-												<p className='current'>{weekItem.format('D')}</p>
-											) : (
-											<p>	{weekItem.format('D')}</p>
-											)
-										}
-                    <ul className="grid__item-event-list">
-                      {
-                        events.filter(event => event.date >= weekItem.format('X') && event.date <= weekItem.clone().endOf('day').format('X'))
-                        .map((ev) => (<li key={ev.id} className={`event__item ${ev.type}`}>{ev.name}</li>) )
-                      }
-                      
-                    </ul>
+        <div
+          key={uniqid()}
+          className={`grid__item ${changeClassName(weekItem)}`}
+        >
+          <div className="grid__item-card">
+            {isCurrentDay(weekItem) ? (
+              <p className="current">{weekItem.format("D")}</p>
+            ) : (
+              <p> {weekItem.format("D")}</p>
+            )}
+            <ul className="grid__item-event-list">
+              {events
+                .filter(
+                  (event) =>
+                    event.date >= weekItem.format("X") &&
+                    event.date <= weekItem.clone().endOf("day").format("X")
+                )
+                .map((ev) => (
+                  <li key={ev.id} className={`event__item ${ev.type}`}>
+                    {ev.name}
+                  </li>
+                ))}
+            </ul>
+          </div>
         </div>
-                    
-        </div>
-      ))
-      }
-    
+      ))}
     </div>
   );
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
-import arrow from '../../assets/img/arrow.svg';
-import moment from 'moment';
-import uniqid from 'uniqid';
+import React from "react";
+import arrow from "../../assets/img/arrow.svg";
+import moment from "moment";
+import uniqid from "uniqid";
 
-export default function Header({ today, prevHandler, todayHandler, nextHandler }) {
+export default function Header({
+  today,
+  prevHandler,
+  todayHandler,
+  nextHandler,
+}) {
   // пустой массив для вывода дней недели
   const nameOfDay = [...Array(7)];
   const isCurrentYear = new Date().getFullYear();
@@ -16,8 +21,12 @@ export default function Header({ today, prevHandler, todayHandler, nextHandler }
         </button>
         <span onClick={todayHandler} className="monthName">
           {/* вывод текущего месяца + выводим год в зависимости от условия*/}
-          {today.format('MMMM') + ' '}
-          {Number(isCurrentYear) === Number(today.format('YYYY')) ? '' : <span>{today.format('YYYY')}</span>}
+          {today.format("MMMM") + " "}
+          {Number(isCurrentYear) === Number(today.format("YYYY")) ? (
+            ""
+          ) : (
+            <span>{today.format("YYYY")}</span>
+          )}
         </span>
         <button className="btn__changeMonth" onClick={nextHandler}>
           <img src={arrow} alt="" />
@@ -27,7 +36,7 @@ export default function Header({ today, prevHandler, todayHandler, nextHandler }
       <div className="header__days">
         {nameOfDay.map((i, index) => (
           <div key={uniqid()} className="header__item">
-            {moment().day(index + 1).format('dddd')}
+            {moment().day(index + 1).format("dddd")}
           </div>
         ))}
       </div>
